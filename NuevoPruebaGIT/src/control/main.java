f60b17f332332e1c56af1b7113b90035daf8c04f
Eduardo Arroyo <>
2020-02-10 19:21:38 +0100

    Esta es la modificacion del documento Main solo he agregado una nueva linea pirntln

--- NuevoPruebaGIT/src/control/main.java
+++ NuevoPruebaGIT/src/control/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Este es el ejemplo de prueba de EDU");
+		System.out.println("Entro en la modificacion por la branch development");
 	}
 
 }
