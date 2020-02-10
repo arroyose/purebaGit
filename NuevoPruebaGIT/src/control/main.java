ec7d87db2ecf90d3cc0b854c4044d0c5467d1875
yusi <yusi118@gmail.com>
2020-02-10 21:46:11 +0100

    Cambio datos

--- NuevoPruebaGIT/src/control/main.java
+++ NuevoPruebaGIT/src/control/main.java
@@ -9,11 +9,8 @@ public class main {
 		System.out.println("Entro en la rama edu_tarea1");
 		System.out.println("Estoy rama yusi_tarea");
 		System.out.println(" estamos dentro?");
-<<<<<<< HEAD
 		System.out.println("ESTE ES NUEVO DE EDUs");
-=======
 		System.out.println("estoy en la rama otra vez ");
->>>>>>> e60aedf40526c228d81f158984a6fff937178678
 	}
 
 }
