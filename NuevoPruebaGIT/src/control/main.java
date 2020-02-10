81d41b48f066852dc69260004017c000dbad2978
Eduardo Arroyo <>
2020-02-10 20:20:37 +0100

    Este es el de tarea edu 1

--- NuevoPruebaGIT/src/control/main.java
+++ NuevoPruebaGIT/src/control/main.java
@@ -6,6 +6,7 @@ public class main {
 		// TODO Auto-generated method stub
 		System.out.println("Este es el ejemplo de prueba de EDU");
 		System.out.println("Entro en la modificacion por la branch development");
+		System.out.println("Entro en la rama edu_tarea1");
 	}
 
 }
