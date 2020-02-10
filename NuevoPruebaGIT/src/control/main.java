996aff912fb1fe34f3c19ea0fd7812b6b9cac153
Eduardo Arroyo <>
2020-02-10 19:13:45 +0100

    Este es la carga inicial del proyecto en la rama master

---
+++ NuevoPruebaGIT/src/control/main.java
@@ -0,0 +1,10 @@
+package control;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este es el ejemplo de prueba de EDU");
+	}
+
+}
