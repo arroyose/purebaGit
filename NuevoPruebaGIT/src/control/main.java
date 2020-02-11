762d3521bfdc77f1904e8963b2c28ee9ecc31b1d
Eduardo Arroyo <>
2020-02-11 07:45:02 +0100

    Cambios en el main para la ejecucion segunda Linea

--- NuevoPruebaGIT/src/control/main.java
+++ NuevoPruebaGIT/src/control/main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		new Test().inicializar();
 		System.out.println("E modificado el main");
+		System.out.println("Esta es la segund liena");
 	}
 
 }
