5437dc3f49531455fe4f424cab6825875e7306d8
yusi <yusi118@gmail.com>
2020-02-11 12:03:03 +0100

    Crear la clase servicio en paquete servicio

---
+++ NuevoPruebaGIT/src/servicios/ImpServicios.java
@@ -0,0 +1,15 @@
+package servicios;
+import datos.IDatos;
+public class ImpServicios implements IServicios {
+	@Override
+	public void addPeliculas() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void listarPeliculas() throws Exception {
+		
+	}
+
+}
