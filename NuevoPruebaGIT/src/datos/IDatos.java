a8bc25e3565c3ad0a6a46c8cbb1b2b09d5f89fef
German Sanchez <luqh555@gmail.com>
2020-02-11 15:56:24 +0100

    correcion conflictos

--- NuevoPruebaGIT/src/datos/IDatos.java
+++ NuevoPruebaGIT/src/datos/IDatos.java
@@ -9,9 +9,6 @@ public interface IDatos {
 
 	public void addPeliculas (Peliculas pelicula) throws Exception;
 	public void listarPeliculas() throws Exception;
-<<<<<<< HEAD
+
 	public void addSocio(Socios socio) throws Exception;
-=======
-	public void addSocio(Socios s) throws Exception;
->>>>>>> 912c345d6e5d7bcb9c9bf5df5978d48edf4fc8d4
 }
--- NuevoPruebaGIT/src/servicios/ImpServicios.java
+++ NuevoPruebaGIT/src/servicios/ImpServicios.java
@@ -3,42 +3,34 @@ import datos.IDatos;
 import datos.ImpDatos;
 import modelos.Peliculas;
 import modelos.Socios;
-<<<<<<< HEAD
 
 
 
 
-=======
->>>>>>> 912c345d6e5d7bcb9c9bf5df5978d48edf4fc8d4
+
+
+
 public class ImpServicios implements IServicios {
 	
 	private IDatos datos=new ImpDatos();
 	
 	
 	@Override
-<<<<<<< HEAD
+
 	public void addPeliculas(Peliculas pelicula) throws Exception {
 		// TODO Auto-generated method stub
       datos.addPeliculas(pelicula);
-=======
-	public void addPeliculas(Peliculas p) throws Exception {
-		datos.addPeliculas(p);
-			
->>>>>>> 912c345d6e5d7bcb9c9bf5df5978d48edf4fc8d4
 	}
 	@Override
 	public void listarPeliculas() throws Exception {
 		 
 		datos.listarPeliculas();
 	}
-<<<<<<< HEAD
+
 
 	public void addSocios(Socios socio) throws Exception{
 		datos.addSocio(socio);
-=======
+
 	
-	public void addSocio(Socios s) throws Exception{
-		datos.addSocio(s);
->>>>>>> 912c345d6e5d7bcb9c9bf5df5978d48edf4fc8d4
 	}
 }
