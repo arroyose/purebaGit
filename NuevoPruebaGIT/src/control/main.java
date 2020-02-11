d378419e6a09768f49019cbbc2e9673684837168
yusi <yusi118@gmail.com>
2020-02-11 08:04:20 +0100

    No changes

--- NuevoPruebaGIT/src/control/main.java
+++ NuevoPruebaGIT/src/control/main.java
@@ -1,28 +1,11 @@
 package control;
 
-<<<<<<< HEAD
-public class Main{
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Este es el ejemplo de prueba de EDU");
-		System.out.println("Entro en la modificacion por la branch development");
-		System.out.println("Entro en la rama edu_tarea1");
-		System.out.println("Estoy rama yusi_tarea");
-		System.out.println(" estamos dentro?");
-		System.out.println("ESTE ES NUEVO DE EDUs");
-		System.out.println("estoy en la rama otra vez ");
-
-		new Test().inicializar();
-
-=======
-public class Main {
+public class main {
 
 	public static void main(String[] args) {
 		new Test().inicializar();
 		System.out.println("E modificado el main");
 		System.out.println("Esta es la segund liena");
->>>>>>> 762d3521bfdc77f1904e8963b2c28ee9ecc31b1d
 	}
 
 }
