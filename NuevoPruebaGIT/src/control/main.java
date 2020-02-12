4f5af72f890ad67e6082f905a067aa4896bbdd86
German Sanchez <luqh555@gmail.com>
2020-02-12 12:11:00 +0100

    movieflix

--- NuevoPruebaGIT/src/control/main.java
+++ NuevoPruebaGIT/src/control/main.java
@@ -1,16 +1,15 @@
 package control;
+import gui.Menu;
 
 public class main {
 
 	public static void main(String[] args) {
-		Test t1= new Test();
 		
-		t1.inicializar();
-		t1.crearSocio();
 		
-		System.out.println("E modificado el main");
-		System.out.println("Esta es la segund liena");
-		System.out.println("Esta es germnan");
+		new movieflix().mostrarMovieflix();
+		
+		
+		
 	}
 
 }
