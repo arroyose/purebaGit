8563092103945220f3ae5590f58093bff3954322
Eduardo Arroyo <>
2020-02-12 14:14:50 +0100

    Esto es el nuevo commit con el .classpth modificado en funcionamiento

--- NuevoPruebaGIT/src/control/main.java
+++ NuevoPruebaGIT/src/control/main.java
@@ -4,8 +4,7 @@ package control;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Edu");
-		//new movieflix().mostrarMovieflix();
+		new movieflix().mostrarMovieflix();
 
 	}
 
